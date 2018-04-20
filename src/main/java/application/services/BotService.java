ee36bb291d5b9bdfc737c0112516ae14f23c75b9
Илья Москалев <ilamoskalev@MacBook-Pro-Ila.local>
2018-04-21 02:39:57 +0300

    done

--- src/main/java/application/services/BotService.java
+++ src/main/java/application/services/BotService.java
@@ -5,9 +5,9 @@ import application.dao.user.UserDAO;
 import application.entities.CommandEntity;
 import application.entities.UserEntity;
 import application.exceptions.WrongTokenExeption;
-import application.requests.Command;
 import application.requests.Message;
 import application.responces.MessageResponce;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -35,11 +35,33 @@ public class BotService {
         final String message = body.getMessage();
         for (CommandEntity command : commands) {
             for (String flag : command.getDescription()) {
-                if (message.contains(flag)){
+                if (flag.contains("%")) {
+                    if (message.contains(flag.substring(0, flag.indexOf('%')))) {
+                        return new ResponseEntity<>(
+                                new MessageResponce(
+                                        command.getUrl() + '?'
+                                                + getParam(flag) + '='
+                                                + getValue(message, flag.substring(0, flag.indexOf('%')))
+                                ), HttpStatus.OK);
+                    }
+                } else if (message.contains(flag)) {
                     return new ResponseEntity<>(new MessageResponce(command.getUrl()), HttpStatus.OK);
                 }
             }
         }
         return new ResponseEntity<>(new MessageResponce("not found"), HttpStatus.OK);
+    }
+
+    private String getParam(@NotNull String flag) {
+        String tmp = flag.substring(flag.indexOf('%') + 1, flag.length());
+        return tmp.substring(0, tmp.indexOf('%'));
+    }
+
+    private String getValue(@NotNull String message, @NotNull String flag) {
+        String tmp = message.substring(message.indexOf(flag) + flag.length());
+        if (tmp.indexOf(' ') > 0) {
+            return tmp.substring(0, tmp.indexOf(" "));
+        }
+        return tmp;
     }
 }
